a25a45a78ce804b9d8caa4d8b948a2c1ae12f7c8
= <=>
2025-01-28 12:47:34 -0300

    stattic

--- src/app.ts
+++ src/app.ts
@@ -20,6 +20,7 @@ class App {
     this.express.use(express.urlencoded({ extended: true }))
     this.express.use(cors())
     this.express.use(morgan('dev'))
+    this.express.use(express.static('public'))
   }
 
   private routes (): void {
