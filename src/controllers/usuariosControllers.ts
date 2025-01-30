04e299c72c30799bf63aeed8fd0151e2e274733f
= <=>
2025-01-30 14:53:30 -0300

    CONTORLADER create/update TODOS LOS USUARIOS

--- src/controllers/usuariosControllers.ts
+++ src/controllers/usuariosControllers.ts
@@ -2,9 +2,9 @@ import { Request, Response } from 'express'
 import Usuario from '../models/usuarioModels'
 
 // Obtener todos los usuarios
-export const getUsuarios = (_req: Request, res: Response): void => {
+export const getUsuarios = async (_req: Request, res: Response): Promise<void> => {
   try {
-    const usuarios = Usuario.findAll()
+    const usuarios = await Usuario.findAll()
     res.status(200).json(usuarios)
   } catch (error) {
     if (error instanceof Error) {
@@ -14,23 +14,54 @@ export const getUsuarios = (_req: Request, res: Response): void => {
   }
 }
 
-export const getUsuario = (req: Request, res: Response): void => {
+export const getUsuario = async (req: Request, res: Response): Promise<void> => {
   const { id } = req.params
-  res.send('Obtener un solo usuario' + id)
+  try {
+    const usuario = await Usuario.findByPk(id)
+    if (usuario == null) {
+      res.status(404).send(`No se encuentra el id ${id}`)
+    } else {
+      res.status(200).json(usuario)
+    }
+  } catch (error) {
+    if (error instanceof Error) {
+      console.error('Error al consultar la base de datos')
+      res.status(500).send(error.message)
+    }
+  }
 }
 
-export const postUsuario = (req: Request, res: Response): void => {
-  const { id } = req.params
+export const postUsuario = async (req: Request, res: Response): Promise<void> => {
   const body = req.body
-  console.log(body)
-  res.send('Crear un nuevo usuario' + id)
+  try {
+    const usuario = Usuario.build(body)
+    await usuario.save()
+    res.status(201).json(usuario)
+  } catch (error) {
+    if (error instanceof Error) {
+      console.error('Error al consultar la base de datos')
+      res.status(500).send(error.message)
+    }
+  }
 }
 
-export const updateUsuario = (req: Request, res: Response): void => {
+export const updateUsuario = async (req: Request, res: Response): Promise<void> => {
   const { id } = req.params
   const body = req.body
-  console.log(body)
-  res.send('Actualizar usuario' + id)
+  try {
+    const usuario = await Usuario.findByPk(id)
+    if (usuario == null) {
+      throw new Error('No se ecnuetra usuario')
+    } else {
+      const updateUser = await usuario.update(body)
+      res.status(201).json(updateUser)
+    }
+  } catch (error) {
+    if (error instanceof Error) {
+      console.error('Error al consultar la base de datos')
+      res.status(500).send(error.message)
+    }
+  }
 }
 
 export const deleteUsuario = (req: Request, res: Response): void => {
